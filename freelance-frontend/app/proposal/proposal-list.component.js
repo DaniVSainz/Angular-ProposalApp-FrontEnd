31eef0c5825e21523a6691dae28277ed8f65b6f0
DaniVSainz <danivsainz@gmail.com>
2017-04-27 17:38:10 -0400

    Proposal component hardcoded but ready for api"

--- freelance-frontend/app/proposal/proposal-list.component.js
+++ freelance-frontend/app/proposal/proposal-list.component.js
@@ -9,8 +9,17 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var proposal_1 = require('./proposal');
 var ProposalListComponent = (function () {
     function ProposalListComponent() {
+        this.proposalOne = new proposal_1.Proposal(15, 'Abc Company', 'http://google.com', 'Ruby on rails', 150, 120, 15, "danivsainz@gmail.com");
+        this.proposalTwo = new proposal_1.Proposal(99, 'qwetc Company', 'http://google.com', 'Ruby on rails', 150, 120, 15, "danivsainz@gmail.com");
+        this.proposalThree = new proposal_1.Proposal(150, 'hfg Company', 'http://google.com', 'Ruby on rails', 150, 120, 15, "danivsainz@gmail.com");
+        this.proposals = [
+            this.proposalOne,
+            this.proposalTwo,
+            this.proposalThree
+        ];
     }
     ProposalListComponent = __decorate([
         core_1.Component({
--- freelance-frontend/app/proposal/proposal-list.component.ts
+++ freelance-frontend/app/proposal/proposal-list.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal'
 
 @Component({
   moduleId: module.id,
   selector: 'proposal-list',
   templateUrl: 'proposal-list.component.html'
 })
-export class ProposalListComponent {}
+export class ProposalListComponent {
+  proposalOne: Proposal = new Proposal(15, 'Abc Company', 'http://google.com', 'Ruby on rails', 150, 120, 15,"danivsainz@gmail.com" )
+  proposalTwo: Proposal = new Proposal(99, 'qwetc Company', 'http://google.com', 'Ruby on rails', 150, 120, 15,"danivsainz@gmail.com" )
+  proposalThree: Proposal = new Proposal(150, 'hfg Company', 'http://google.com', 'Ruby on rails', 150, 120, 15,"danivsainz@gmail.com" )
+  proposals: Proposal[]= [
+    this.proposalOne,
+    this.proposalTwo,
+    this.proposalThree
+  ]
+}
