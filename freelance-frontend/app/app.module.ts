573e87aa3559cfd45eff1c211f621949cca640ff
DaniVSainz <danivsainz@gmail.com>
2017-04-28 23:55:20 -0400

    add app.component.css + add to app.component .ts

--- freelance-frontend/app/app.module.ts
+++ freelance-frontend/app/app.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { AppRoutingModule } from './app-routing.module';
-
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
