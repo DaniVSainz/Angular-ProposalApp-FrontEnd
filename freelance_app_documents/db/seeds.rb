777cac67b41456956862b1319c7d6623edf35a21
DaniVSainz <danivsainz@gmail.com>
2017-04-29 03:31:28 -0400

    Create seed loop

--- freelance_app_documents/db/seeds.rb
+++ freelance_app_documents/db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+  # create_table "freelance_documents", force: :cascade do |t|
+  #   t.string "title"
+  #   t.string "description"
+  #   t.text "file_url"
+  #   t.text "image_url"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  # end
+
+
+
+10.times do |d| 
+	FreelanceDocument.create( 
+	title: "Document #{d}",
+	description: 'Vestibulum tempus vestibulum risus in iaculis. Suspendisse potenti. Curabitur lectus dui, lacinia eget est ac, blandit semper felis. Nullam aliquet
+	scelerisque purus nec aliquam. Praesent maximus, quam et semper cursus, lectus mi tristique enim,
+	eu accumsan arcu arcu nec augue. Mauris quis urna at sapien malesuada aliquet at in dui',
+	file_url: "https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit?usp=sharing",
+	image_url: 'https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg'
+	)
+
+	end 
